aba723986012cf06bb42fa226c9677fe8d932caf
Abhishek Sharma <abhisheks@berkeley.edu>
2019-11-08 00:44:46 -0800

    Removed Row Class from Models

--- courserec/models.py
+++ courserec/models.py
@@ -3,8 +3,3 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
-
-class Row(models.Model):
-    name = models.CharField(max_length=10)
-    description = models.CharField(max_length=50)
-    difficulty = models.CharField(max_length=10)
